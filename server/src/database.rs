5fe7208b6cf406523c42c6280ef722a634e5aff4
PeterYurkovich <pyurkovi@redhat.com>
2025-04-17 22:25:07 -0400

    feat: add high_score table

--- server/src/database.rs
+++ server/src/database.rs
@@ -3,12 +3,22 @@ use rusqlite_migration::{M, Migrations, Result};
 
 pub fn migrate() -> Result<()> {
     // 1️⃣ Define migrations
-    let migrations = Migrations::new(vec![M::up(
-        "CREATE TABLE IF NOT EXISTS maze (
-        key   TEXT PRIMARY KEY,
-        maze  BLOB NOT NULL
-    );",
-    )]);
+    let migrations = Migrations::new(vec![
+        M::up(
+            "CREATE TABLE IF NOT EXISTS maze (
+                key   TEXT PRIMARY KEY,
+                maze  BLOB NOT NULL
+            );",
+        ),
+        M::up(
+            "CREATE TABLE IF NOT EXISTS high_score (
+                id      INTEGER PRIMARY KEY,
+                key     TEXT REFERENCES maze (key) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                time    TEXT,
+                user    TEXT
+            );",
+        ),
+    ]);
 
     let mut connection = Connection::open("./sqlite.db3")?;
     // 2️⃣ Update the database schema, atomically
